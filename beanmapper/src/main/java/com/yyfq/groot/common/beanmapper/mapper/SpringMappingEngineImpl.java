25721ac74c8e315433139bd1cb2eaf940a5af08b
junqing.li <junqing.li@mljr.com>
2017-07-21 19:23:42 +0800

    null转换优化

--- beanmapper/src/main/java/com/yyfq/groot/common/beanmapper/mapper/SpringMappingEngineImpl.java
+++ beanmapper/src/main/java/com/yyfq/groot/common/beanmapper/mapper/SpringMappingEngineImpl.java
@@ -1,5 +1,7 @@
 package com.yyfq.groot.common.beanmapper.mapper;
 
+import java.util.Objects;
+
 import org.springframework.beans.BeanUtils;
 
 /**
@@ -9,6 +11,10 @@ public class SpringMappingEngineImpl extends AbstractMappingEngine {
 
   @Override
   protected <T, S> T mapInternal(S source, T target) {
+
+    if (Objects.isNull(source)) {
+      return null;
+    }
 
     BeanUtils.copyProperties(source, target);
 
